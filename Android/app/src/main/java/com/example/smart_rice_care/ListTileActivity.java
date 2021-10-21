adb8c0391447f20b5623f893b2f22dcc6dabbefe
RusJ <rjillesingha@gmail.com>
2021-10-22 02:04:58 +0530

    farmer requests UI

---
+++ Android/app/src/main/java/com/example/smart_rice_care/ListTileActivity.java
@@ -0,0 +1,14 @@
+package com.example.smart_rice_care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListTileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_tile);
+    }
+}
