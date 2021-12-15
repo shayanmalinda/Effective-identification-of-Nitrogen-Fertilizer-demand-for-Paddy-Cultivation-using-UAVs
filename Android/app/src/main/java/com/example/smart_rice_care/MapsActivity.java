7749d0662222ca36f009fc90853806a2fa70032d
shayanmalinda <shayanmalinda@gmail.com>
2021-12-15 18:34:48 +0530

    Fetching FieldData and plotting the density map

--- Android/app/src/main/java/com/example/smart_rice_care/MapsActivity.java
+++ Android/app/src/main/java/com/example/smart_rice_care/MapsActivity.java
@@ -23,11 +23,13 @@ import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.maps.android.heatmaps.Gradient;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
@@ -46,10 +48,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    String uId;
+    List<FieldData> fieldData = new ArrayList<>();
+
+    List<WeightedLatLng> level2List = new ArrayList<>();
+    List<WeightedLatLng> level3List = new ArrayList<>();
+    List<WeightedLatLng> level4List = new ArrayList<>();
+
+    int[] level2Colors = {
+            Color.rgb(255, 0, 0),
+            Color.rgb(255, 0, 0)
+    };
+    int[] level3Colors = {
+            Color.rgb(255, 255, 26),
+            Color.rgb(255, 255, 26)
+    };
+    int[] level4Colors = {
+            Color.rgb(68, 204, 0),
+            Color.rgb(68, 204, 0)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        uId = mAuth.getInstance().getUid();
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -64,12 +91,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(mMap.MAP_TYPE_SATELLITE);
-        LatLng marker = new LatLng(6.0718297, 80.23611111111111);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
 
         mMap.setMinZoomPreference(20.0f);
         mMap.setMaxZoomPreference(50.0f);
-        addHeatMap();
+
+        LatLng marker = new LatLng(6.0718297, 80.23611111111111);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
 
         fetchFieldData();
     }
@@ -78,39 +105,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Access a Cloud Firestore instance from your Activity
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        db.collection("field_data").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable @org.jetbrains.annotations.Nullable QuerySnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
-                if(error!=null){
-                    Log.e("Fetching Field Data",error.getMessage());
-                }
-                else{
-                    for(DocumentSnapshot doc : value){
-                        Log.d("Fetching Field Data", doc.getString("request_id"));
+        db.collection("FieldData")
+                .whereEqualTo("level",2)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable @org.jetbrains.annotations.Nullable QuerySnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+                            Log.w("Fetching Error", "Listen failed.", error);
+                            Toast.makeText(MapsActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        for (QueryDocumentSnapshot doc : value) {
+                            Double latitude = doc.getDouble("latitude");
+                            Double longitude = doc.getDouble("longitude");;
+                            Integer level = doc.getLong("level").intValue();
+                            String officerId = doc.getString("officerId");
+                            String requestId = doc.getString("requestId");
+                            FieldData obj = new FieldData(latitude, longitude, level, officerId, requestId, level);
+                            fieldData.add(obj);
+
+                            LatLng latLng = new LatLng(latitude, longitude);
+                            WeightedLatLng weightedLatLngObj = new WeightedLatLng(latLng, 1);
+                            level2List.add(weightedLatLngObj);
+                        }
+
+                        addHeatMap(level2List, level2Colors);
                     }
-                }
-            }
-        });
+                });
+
+
+        db.collection("FieldData")
+                .whereEqualTo("level",3)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable @org.jetbrains.annotations.Nullable QuerySnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+                            Log.w("Fetching Error", "Listen failed.", error);
+                            Toast.makeText(MapsActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        for (QueryDocumentSnapshot doc : value) {
+                            Double latitude = doc.getDouble("latitude");
+                            Double longitude = doc.getDouble("longitude");;
+                            Integer level = doc.getLong("level").intValue();
+                            String officerId = doc.getString("officerId");
+                            String requestId = doc.getString("requestId");
+                            FieldData obj = new FieldData(latitude, longitude, level, officerId, requestId, level);
+                            fieldData.add(obj);
+
+                            LatLng latLng = new LatLng(latitude, longitude);
+                            WeightedLatLng weightedLatLngObj = new WeightedLatLng(latLng, 1);
+                            level3List.add(weightedLatLngObj);
+                        }
+
+                        addHeatMap(level3List, level3Colors);
+                    }
+                });
+
+
+
+        db.collection("FieldData")
+                .whereEqualTo("level",4)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable @org.jetbrains.annotations.Nullable QuerySnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+                            Log.w("Fetching Error", "Listen failed.", error);
+                            Toast.makeText(MapsActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        for (QueryDocumentSnapshot doc : value) {
+                            Double latitude = doc.getDouble("latitude");
+                            Double longitude = doc.getDouble("longitude");;
+                            Integer level = doc.getLong("level").intValue();
+                            String officerId = doc.getString("officerId");
+                            String requestId = doc.getString("requestId");
+                            FieldData obj = new FieldData(latitude, longitude, level, officerId, requestId, level);
+                            fieldData.add(obj);
+
+                            LatLng latLng = new LatLng(latitude, longitude);
+                            WeightedLatLng weightedLatLngObj = new WeightedLatLng(latLng, 1);
+                            level4List.add(weightedLatLngObj);
+                        }
+
+                        addHeatMap(level4List, level4Colors);
+                    }
+                });
+
 
     }
 
-    private void addHeatMap() {
-        List<WeightedLatLng> data = null;
+    private void addHeatMap(List<WeightedLatLng> weightedList,int[] colors) {
 
-        // Get the data: latitude/longitude positions of police stations.
-        try {
-            data = readItems(R.raw.test);
-            Log.i("Data length", String.valueOf(data.size()));
-        } catch (JSONException e) {
-            Log.e("Err","Problem reading list of locations.");
-            Toast.makeText(this, "Problem reading list of locations.", Toast.LENGTH_LONG).show();
-        }
-
-
-        int[] colors = {
-                Color.rgb(0, 255, 0),
-                Color.rgb(0, 255, 0)
-        };
 
         float[] startPoints = {
                 0.6f, 1f
@@ -120,62 +206,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Create a heat map tile provider, passing it the latlngs of the police stations.
         HeatmapTileProvider provider = new HeatmapTileProvider.Builder()
-                .weightedData(data)
+                .weightedData(weightedList)
                 .gradient(gradient)
-                .radius(50)
+                .radius(40)
                 .opacity(1)
                 .build();
 
         // Add a tile overlay to the map, using the heat map tile provider.
         TileOverlay overlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
     }
-
-    private List<WeightedLatLng> readItems(@RawRes int resource) throws JSONException {
-        List<WeightedLatLng> result = new ArrayList<>();
-        InputStream inputStream = this.getResources().openRawResource(resource);
-        String json = new Scanner(inputStream).useDelimiter("\\A").next();
-        JSONArray array = new JSONArray(json);
-        for (int i = 0; i < array.length(); i++) {
-            JSONObject object = array.getJSONObject(i);
-            double lat = object.getDouble("lat");
-            Log.i("Lat=>",lat+"");
-            double lng = object.getDouble("lon");
-            double density = object.getDouble("density");
-            result.add(new WeightedLatLng(new LatLng(lat,lng), density));
-        }
-        return result;
-    }
-
-//    private void addHeatMap() {
-//        List<LatLng> latLngs = null;
-//
-//        // Get the data: latitude/longitude positions of police stations.
-//        try {
-//            latLngs = readItems(R.raw.police_stations);
-//        } catch (JSONException e) {
-//            Toast.makeText(this, "Problem reading list of locations.", Toast.LENGTH_LONG).show();
-//        }
-//
-//        // Create a heat map tile provider, passing it the latlngs of the police stations.
-//        HeatmapTileProvider provider = new HeatmapTileProvider.Builder()
-//                .data(latLngs)
-//                .build();
-//
-//        // Add a tile overlay to the map, using the heat map tile provider.
-//        TileOverlay overlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
-//    }
-
-//    private List<LatLng> readItems(@RawRes int resource) throws JSONException {
-//        List<LatLng> result = new ArrayList<>();
-//        InputStream inputStream = this.getResources().openRawResource(resource);
-//        String json = new Scanner(inputStream).useDelimiter("\\A").next();
-//        JSONArray array = new JSONArray(json);
-//        for (int i = 0; i < array.length(); i++) {
-//            JSONObject object = array.getJSONObject(i);
-//            double lat = object.getDouble("lat");
-//            double lng = object.getDouble("lng");
-//            result.add(new LatLng(lat, lng));
-//        }
-//        return result;
-//    }
 }
