0c6e311d42dc60741360f740dd70b2e1b0987b84
AnupamaTK <swathathsarani@gmail.com>
2021-12-23 21:41:00 +0530

    Field Details

--- Web/src/app/pages/field-details/field-details.component.ts
+++ Web/src/app/pages/field-details/field-details.component.ts
@@ -1,6 +1,8 @@
+import { FieldRequest } from './../../models/field-request.model';
 import { Component, OnInit } from '@angular/core';
 import { Field } from '../../models/field.model';
 import { Router } from '@angular/router';
+import { FieldRequestService } from '../../services/field-request.service';
 
 @Component({
   selector: 'app-field-details',
@@ -13,16 +15,24 @@ export class FieldDetailsComponent implements OnInit {
   title = 'My first AGM project';
   lat = 51.678418;
   lng = 7.809007;
+  fieldRequests: FieldRequest[];
+  counts: any[];
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private fieldRequestService: FieldRequestService) {
     this.field = this.router.getCurrentNavigation().extras.state.field;
 
   }
 
   ngOnInit() {
+    // this.fieldRequestService.getFieldVisitRequests(this.field.id).subscribe(data => {
+    //   this.fieldRequests = data;
+    // });
+    this.counts = this.fieldRequestService.getFieldVisitCountsByStatus(this.field.id);
+    // console.log(this.counts);
+    // });
 
   }
- 
+
 
 }
 
--- Web/src/app/services/field.service.ts
+++ Web/src/app/services/field.service.ts
@@ -2,7 +2,6 @@ import { User } from './../models/user.model';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Field } from '../models/field.model';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,40 +15,12 @@ export class FieldService {
 
   constructor(private fireStore: AngularFirestore) { }
 
-  // getFields(): Observable<any> {
-  //   return Observable.create(observer => {
-  //     this.fireStore.collection('FieldDetails').snapshotChanges().subscribe(data => {
-
-  //       data.map(e => {
-  //         this.data = e.payload.doc.data() as Field;
-  //         this.fireStore.collection('Users').doc(this.data.farmerId).snapshotChanges().subscribe(
-  //           res => {
-  //             this.farmer = { ...res.payload.data() as User };
-  //             return {
-  //               id: e.payload.doc.id,
-  //               farmer: this.farmer.firstName + " " + this.farmer.lastName,
-  //               ...e.payload.doc.data() as {}
-  //             } as Field;
-  //           }
-  //         )
-  //       })
-  //     });
-  //   });
-
-  //   // return this.fields;
-
-
-  // }
   getFields() {
     return this.fireStore.collection('FieldDetails').snapshotChanges();
+  }
  
-  }
-  getField(fieldId: String) {
-    return this.fireStore.collection('FieldDetails', ref => ref.where('nic', '==', '867286151V')).get();
-  }
 
   deleteField(fieldId: String) {
-    console.log(fieldId);
     this.fireStore.doc('FieldDetails/' + fieldId).delete();
   }
 
