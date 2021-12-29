6d7195e52aed507125f6cd37d5f375b0996db67c
AnupamaTK <swathathsarani@gmail.com>
2021-12-29 15:18:11 +0530

    Field Details Updated

--- Web/src/app/pages/field-details/field-details.component.ts
+++ Web/src/app/pages/field-details/field-details.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Field } from '../../models/field.model';
 import { Router } from '@angular/router';
 import { FieldRequestService } from '../../services/field-request.service';
+import { reduce } from 'rxjs-compat/operator/reduce';
 
 @Component({
   selector: 'app-field-details',
@@ -17,10 +18,14 @@ export class FieldDetailsComponent implements OnInit {
   lng = 7.809007;
   fieldRequests: FieldRequest[];
   counts: any[];
+  valueChanged;
 
   constructor(private router: Router, private fieldRequestService: FieldRequestService) {
     this.field = this.router.getCurrentNavigation().extras.state.field;
 
+  }
+  getColor(status) {
+    if (status == 'pending') return '[bg-pending]'
   }
 
   ngOnInit() {
