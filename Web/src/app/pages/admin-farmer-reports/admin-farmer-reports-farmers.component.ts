4048125a1d0c8db9288a630be3c383eea592a43e
AnupamaTK <swathathsarani@gmail.com>
2022-04-21 19:04:47 +0530

    Field Visit Req report

--- Web/src/app/pages/admin-farmer-reports/admin-farmer-reports-farmers.component.ts
+++ Web/src/app/pages/admin-farmer-reports/admin-farmer-reports-farmers.component.ts
@@ -14,9 +14,11 @@ import { DatePipe } from '@angular/common';
 import { UserFarmersService } from 'app/services/user-farmers.service';
 import { Field } from 'app/models/field.model';
 import { FieldService } from 'app/services/field.service';
+import { FieldVisitService } from 'app/services/field-visit.service';
 import { UserService } from '../../services/user.service';
 import divisionalDataFile from '../../../assets/jsonfiles/divisions.json';
 import { Router } from '@angular/router';
+import { FieldVisitReqTemp } from '../../models/field-visit.model';
 
 @Component({
   selector: 'app-admin-farmer-reports-farmers',
@@ -42,6 +44,7 @@ export class AdminFarmerReportsComponent implements OnInit {
   provinceCount;
   districtCount;
   divisionCount;
+  fieldvisits: FieldVisitReqTemp[];
   tempCounts = [0, 0, 0, 0];
   reportType = "Farmers Report";
   changedWeekDetails: LCCWeekDetails[];
@@ -55,6 +58,7 @@ export class AdminFarmerReportsComponent implements OnInit {
   pending = 0;
   all = 0;
   fields: Field[];
+  fieldvisitReqs: FieldVisitReqTemp[];
   passedUser: User;
   length = false;
   totalFarmers;
@@ -71,7 +75,7 @@ export class AdminFarmerReportsComponent implements OnInit {
   selections = ['Provinces', 'Districts', 'Divisions', 'Province', 'District'];
   // dataSource: MatTableDataSource<User>;
 
-  constructor(private router: Router, private userService: UserService, private datepipe: DatePipe, private userFarmersService: UserFarmersService, private fieldService: FieldService) {
+  constructor(private router: Router, private userService: UserService, private datepipe: DatePipe, private userFarmersService: UserFarmersService, private fieldService: FieldService, private fieldVisitService: FieldVisitService) {
     this.date = this.datepipe.transform((new Date), 'MMM d, y').toString();
     this.optionSelected = 'Provinces'
     this.displayedColumns = ['key', 'value'];
@@ -90,6 +94,10 @@ export class AdminFarmerReportsComponent implements OnInit {
       this.documentName = "Field Report";
       this.name = "Fields";
 
+    } else if (this.role == 'field visit req') {
+      this.documentName = "Field Visit Request Report";
+      this.name = "Requests";
+
     }
 
   }
@@ -104,14 +112,12 @@ export class AdminFarmerReportsComponent implements OnInit {
     var province = value.toString();
     if (this.optionSelected == 'Provinces') {
       this.provinces.forEach((value: number, key: string) => {
-        console.log(key, value);
         this.values.push({
           key: key,
           value: value
         });
       });
 
-      console.log(this.values)
       this.dataSource = new MatTableDataSource(this.values);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
@@ -121,14 +127,12 @@ export class AdminFarmerReportsComponent implements OnInit {
       this.divisionCount = this.tempCounts[3];
     } else if (this.optionSelected == 'Districts') {
       this.districts.forEach((value: number, key: string) => {
-        console.log(key, value);
         this.values.push({
           key: key,
           value: value
         });
       });
 
-      console.log(this.values)
       this.dataSource = new MatTableDataSource(this.values);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
@@ -164,7 +168,6 @@ export class AdminFarmerReportsComponent implements OnInit {
       this.list = allDistricts.sort();
     }
     else if (this.optionSelected == 'Divisions') { // has to implement
-      console.log('Devision Selected')
       this.clearTable();
       let allDistricts = [];
       let allDivisions = [];
@@ -178,7 +181,6 @@ export class AdminFarmerReportsComponent implements OnInit {
       allDistricts.forEach(dis => {
         // if (dis != "District") {
         val = this.divisionalData["" + dis + ""];
-        console.log(val);
         for (i = 0; i < val.length; i++) {
           allDivisions.push(val[i]);
         }
@@ -187,14 +189,12 @@ export class AdminFarmerReportsComponent implements OnInit {
       this.list = allDivisions.sort; // add divisions to dropdown
 
       this.divisions.forEach((value: number, key: string) => {
-        console.log(key, value);
         this.values.push({
           key: key,
           value: value
         });
       });
 
-      console.log(this.values)
       this.farmerCount = this.tempCounts[0];
       this.provinceCount = this.tempCounts[1];
       this.districtCount = this.tempCounts[2];
@@ -212,30 +212,45 @@ export class AdminFarmerReportsComponent implements OnInit {
     var select = value.toString();
     this.districts.clear();
     this.divisions.clear();
+    if (this.role == 'field visit req') {
+      this.fieldvisitReqs.forEach(e => {
+        console.log(e.province)
+        if (e.province == value)
+          this.districts.set(e.district, this.districts.get(e.district) == null ? 1 : this.districts.get(e.district) + 1);
+        if (e.district == value)
+          this.divisions.set(e.division, this.divisions.get(e.division) == null ? 1 : this.divisions.get(e.division) + 1);
+      })
+    } else {
+      this.users.forEach(e => {
+        if (e.province == value)
+          this.districts.set(e.district, this.districts.get(e.district) == null ? 1 : this.districts.get(e.district) + 1);
+        if (e.district == value)
+          this.divisions.set(e.division, this.divisions.get(e.division) == null ? 1 : this.divisions.get(e.division) + 1);
+      })
+    }
 
-    console.log(value)
-    this.users.forEach(e => {
-      if (e.province == value)
-        this.districts.set(e.district, this.districts.get(e.district) == null ? 1 : this.districts.get(e.district) + 1);
-      if (e.district == value)
-        this.divisions.set(e.division, this.divisions.get(e.division) == null ? 1 : this.divisions.get(e.division) + 1);
-    })
 
     if (this.optionSelected == 'Province') {
       var fCount = 0;
       this.districts.forEach((value: number, key: string) => {
-        console.log(key, value);
         fCount += value;
         this.values.push({
           key: key,
           value: value
         });
-        console.log('Keyyyyy' + key)
-        this.users.forEach(e => {
+        if (this.role == 'field visit req') {
+          this.fieldvisitReqs.forEach(e => {
 
-          if (e.district == key)
-            this.divisions.set(e.division, this.divisions.get(e.division) == null ? 1 : this.divisions.get(e.division) + 1);
-        })
+            if (e.district == key)
+              this.divisions.set(e.division, this.divisions.get(e.division) == null ? 1 : this.divisions.get(e.division) + 1);
+          })
+        } else {
+          this.users.forEach(e => {
+
+            if (e.district == key)
+              this.divisions.set(e.division, this.divisions.get(e.division) == null ? 1 : this.divisions.get(e.division) + 1);
+          })
+        }
       });
       this.provinceCount = 1;
       this.districtCount = this.values.length;
@@ -247,7 +262,6 @@ export class AdminFarmerReportsComponent implements OnInit {
     else {
       var fCount = 0;
       this.divisions.forEach((value: number, key: string) => {
-        console.log(key, value);
         fCount += value;
         this.values.push({
           key: key,
@@ -261,7 +275,6 @@ export class AdminFarmerReportsComponent implements OnInit {
 
     }
 
-    console.log(this.values)
     this.dataSource = new MatTableDataSource(this.values);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
@@ -278,6 +291,7 @@ export class AdminFarmerReportsComponent implements OnInit {
 
   ngOnInit(): void {
     this.tempCounts = [0, 0, 0, 0];
+    this.fieldvisitReqs = [];
 
     if (this.role == 'farmer') {
       this.userService.getActiveUsers(this.role).subscribe(data => {
@@ -291,7 +305,6 @@ export class AdminFarmerReportsComponent implements OnInit {
         this.users.forEach(e => {
           this.fieldService.getFieldofFarmer(e.id).subscribe(f => {
             this.userField = f.map(val => {
-              console.log(val.payload.doc.data())
               return {
                 ...val.payload.doc.data() as {},
               } as Field;
@@ -299,20 +312,10 @@ export class AdminFarmerReportsComponent implements OnInit {
             e.province = this.userField[0].province;
             e.district = this.userField[0].district;
             e.division = this.userField[0].division;
-            console.log(e.district)
             this.provinces.set(e.province, this.provinces.get(e.province) == null ? 1 : this.provinces.get(e.province) + 1);
             this.districts.set(e.district, this.districts.get(e.district) == null ? 1 : this.districts.get(e.district) + 1);
             this.divisions.set(e.division, this.divisions.get(e.division) == null ? 1 : this.divisions.get(e.division) + 1);
 
-            console.log(this.userField[0].province)
-            console.log(this.users)
-
-            // this.users.forEach(e => {
-            //   console.log(e.district)
-            //   this.provinces.set(e.province, this.provinces.get(e.province) == null ? 1 : this.provinces.get(e.province) + 1);
-            //   this.districts.set(e.district, this.districts.get(e.district) == null ? 1 : this.districts.get(e.district) + 1);
-            //   this.divisions.set(e.division, this.divisions.get(e.division) == null ? 1 : this.divisions.get(e.division) + 1);
-            // })
             this.farmerCount = this.users.length;
             this.provinceCount = this.provinces.size;
             this.districtCount = this.districts.size;
@@ -384,13 +387,43 @@ export class AdminFarmerReportsComponent implements OnInit {
 
 
       });
+    } else if (this.role == 'field visit req') {
+      this.fieldvisitReqs = [];
+      this.fieldVisitService.getAllFieldVisitRequests().subscribe(data1 => {
+        this.totalFarmers = data1.length;
+        data1.forEach(e => {
+          var temp = {
+            ...e as FieldVisitReqTemp
+          }
+          this.fieldService.getField(temp.fieldId).subscribe(data => {
+            var tfield = data.payload.data() as Field;
+            if (tfield.status == "active") {
+              temp.division = tfield.division;
+              temp.district = tfield.district;
+              temp.province = tfield.province;
+              this.fieldvisitReqs.push(temp)
+
+              //add counts
+              this.provinces.set(temp.province, this.provinces.get(temp.province) == null ? 1 : this.provinces.get(temp.province) + 1);
+              this.districts.set(temp.district, this.districts.get(temp.district) == null ? 1 : this.districts.get(temp.district) + 1);
+              this.divisions.set(temp.division, this.divisions.get(temp.division) == null ? 1 : this.divisions.get(temp.division) + 1);
+
+            }
+            this.farmerCount = this.fieldvisitReqs.length;
+            this.provinceCount = this.provinces.size;
+            this.districtCount = this.districts.size;
+            this.divisionCount = this.divisions.size;
+            this.tempCounts[0] = this.fieldvisitReqs.length;
+            this.tempCounts[1] = this.provinces.size;
+            this.tempCounts[2] = this.districts.size;
+            this.tempCounts[3] = this.divisions.size;
+
+            this.onOptionSelected('Provinces');
+          })
+          // return temp;
+        })
+      });
     }
-
-
-
   }
 }
-
-
-
 
